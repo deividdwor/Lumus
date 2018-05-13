9451a2d9de8f016bdbb1c230442644a7aded6205
DESKTOP-5LSHH6H\Pichau <deivid.dwor@gmail.com>
2018-05-13 00:35:21 -0300

    desiste de uasr o menu pronto

---
+++ app/src/main/java/com/lumossmart/lumossmarthome/ui/activity/ListaAmbientesActivity.kt
@@ -0,0 +1,13 @@
+package com.lumossmart.lumossmarthome.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.lumossmart.lumossmarthome.R
+
+class ListaAmbientesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_ambientes)
+    }
+}
